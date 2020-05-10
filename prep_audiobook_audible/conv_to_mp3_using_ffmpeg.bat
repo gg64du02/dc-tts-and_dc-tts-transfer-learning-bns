e9b7735615418453dc56c7c919c051699d70eccc
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-05-10 22:27:00 +0200

    .bat to convert the .axx into .mp3

---
+++ prep_audiobook_audible/conv_to_mp3_using_ffmpeg.bat
@@ -0,0 +1,9 @@
+
+REM use this repo to get the activation bytes
+REM https://github.com/inAudible-NG/audible-activator
+
+
+D:\bomberman-fabienpg-ai\dc-tts-tacotron-bns\ffmpeg-20200504-5767a2e-win64-static\ffmpeg-20200504-5767a2e-win64-static\bin\ffmpeg.exe -activation_bytes xxxxxxxx -i "D:\bomberman-fabienpg-ai\dc-tts-tacotron-bns\prep_audiobook_audible\The Poets' Corner.aax" -vn -c:a libmp3lame -v debug ThePoetsCorner.mp3 
+
+
+pause
