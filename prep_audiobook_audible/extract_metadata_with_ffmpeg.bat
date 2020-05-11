cccdbca74ca244ea115e1876818fa97e191dae43
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-05-12 00:02:22 +0200

    Adding another script to extract partially the metadata

---
+++ prep_audiobook_audible/extract_metadata_with_ffmpeg.bat
@@ -0,0 +1,5 @@
+
+
+D:\bomberman-fabienpg-ai\dc-tts-tacotron-bns\ffmpeg-20200504-5767a2e-win64-static\ffmpeg-20200504-5767a2e-win64-static\bin\ffmpeg.exe -activation_bytes xxxxxxxx  -i "D:\bomberman-fabienpg-ai\dc-tts-tacotron-bns\prep_audiobook_audible\The Poets' Corner.aax" -map_metadata 0 -map_metadata:s:v 0:s:v -map_metadata:s:a 0:s:a -f ffmetadata in.txt
+
+pause
