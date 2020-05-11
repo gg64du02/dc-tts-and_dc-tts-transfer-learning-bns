3723eb93973ea81108ca5ade276d86856dbd7b3a
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-05-11 14:32:39 +0200

    Adding a buffering stage to speed up from 80min to 5-15min

--- prep_audiobook_audible/silence_remover.py
+++ prep_audiobook_audible/silence_remover.py
@@ -7,14 +7,14 @@ def remove_sil(path_in, path_out, format="wav"):
     sound = AudioSegment.from_file(path_in, format=format)
     print("sound",sound)
     # print()
-    # tmpAudioSegment = AudioSegment.silent(duration=1000*(6*60+31*60), frame_rate=44100)
+    tmpAudioSegment = AudioSegment.silent(duration=1000*(6*60+31*60), frame_rate=44100)
     # return
 
     tmpSound =  AudioSegment.empty()
     # non_sil_times = detect_nonsilent(sound, min_silence_len=50, silence_thresh=sound.dBFS * 1.5)
     # non_sil_times = detect_nonsilent(sound, min_silence_len=30, silence_thresh=sound.dBFS * 1.5)
     non_sil_times = detect_nonsilent(sound, min_silence_len=10, silence_thresh=sound.dBFS * 1.5)
-    print("non_sil_times",non_sil_times)
+    # print("non_sil_times",non_sil_times)
     if len(non_sil_times) > 0:
         non_sil_times_concat = [non_sil_times[0]]
         if len(non_sil_times) > 1:
@@ -25,18 +25,36 @@ def remove_sil(path_in, path_out, format="wav"):
                     non_sil_times_concat.append(t)
         # non_sil_times = [t for t in non_sil_times_concat if t[1] - t[0] > 350]
         non_sil_times = [t for t in non_sil_times_concat if t[1] - t[0] > 100]
-        print("non_sil_times",non_sil_times)
-        print("len(sound)",len(sound))
-        print("non_sil_times[0][0]: non_sil_times[-1][1]",non_sil_times[0][0],non_sil_times[-1][1])
+        # print("non_sil_times",non_sil_times)
+        # print("len(sound)",len(sound))
+        # print("non_sil_times[0][0]: non_sil_times[-1][1]",non_sil_times[0][0],non_sil_times[-1][1])
         # sound[non_sil_times[0][0]: non_sil_times[-1][1]].export(path_out)
         # print("non_sil_times[0][0]: non_sil_times[-1][1]",non_sil_times[0][0],non_sil_times[0][1])
         # sound[non_sil_times[0][0]: non_sil_times[0][1]].export(path_out)
+        # index = 0
+
+        tmpSoundBuffer = AudioSegment.empty()
+
         for int in non_sil_times:
-            tmpSound += sound[int[0]:int[1]]
+            # tmpSound += sound[int[0]:int[1]]
+            print("tmpSoundBuffer.duration_seconds",tmpSoundBuffer.duration_seconds)
+            if(tmpSoundBuffer.duration_seconds>1000):
+                tmpSound += tmpSoundBuffer
+                tmpSoundBuffer = AudioSegment.empty()
+                print("if(tmpSoundBuffer.duration_seconds>1000):")
+            else:
+                tmpSoundBuffer += sound[int[0]:int[1]]
+                print("!if(tmpSoundBuffer.duration_seconds>1000):")
             print("int",int)
+            print("pc",int[0]/len(sound))
+            # tmpAudioSegment[index:index+(int[1]-int[0])] = sound[int[0]:int[1]]
+            # index += int[1]-int[0]
+            # print("index",index)
         tmpSound.export(path_out)
+        # tmpAudioSegment.export(path_out)
+
 
 # https://audiosegment.readthedocs.io/en/latest/audiosegment.html
 
-# remove_sil("ThePoetsCorner.mp3", "ThePoetsCorner_silence-removed.wav.mp3")
-remove_sil("totakas-song.wav","totakas-song_silence-removed.wav")
+remove_sil("ThePoetsCorner.wav", "ThePoetsCorner_silence-removed.wav")
+# remove_sil("totakas-song.wav","totakas-song_silence-removed.wav")
