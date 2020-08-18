e3904c6ea43916c27c9ce77b9e3241abdb8ac36d
gg64du02 <39301190+gg64du02@users.noreply.github.com>
2020-08-18 20:55:20 +0200

    A script to convert the mp3 to wav

--- prep_audiobook_audible/pydub_convert_mp3_to_wav.py
+++ prep_audiobook_audible/pydub_convert_mp3_to_wav.py
@@ -3,10 +3,10 @@ from os import path
 from pydub import AudioSegment
 
 # files
-# src = "ThePoetsCorner.mp3"
-# dst = "ThePoetsCorner.wav"
-src = "totakas-song.mp3"
-dst = "totakas-song.wav"
+src = "ThePoetsCorner.mp3"
+dst = "ThePoetsCorner.wav"
+# src = "totakas-song.mp3"
+# dst = "totakas-song.wav"
 
 # convert wav to mp3
 sound = AudioSegment.from_mp3(src)
